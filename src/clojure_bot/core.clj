dc6237aa950a297884bfc566fdf0253a113b5362
M4tthewDE <matthias.kronberg@gmail.com>
2023-12-18 23:24:24 +0100

    Replace anon connection with user and token

--- src/clojure_bot/core.clj
+++ src/clojure_bot/core.clj
@@ -2,64 +2,68 @@
   (:import [java.net Socket])
   (:require [clojure.java.io :as jio])
   (:require [clojure.string :as str])
+  (:require [cheshire.core :as json])
   (:gen-class))
 
-;; parser
-(defrecord Message [msg-type channel user content])
+(defrecord Message [msg-type channel user content]
+  Object
+  (toString [this]
+    (str "msg-type: " msg-type ", channel: " channel ", user: " user ", content: " content)))
 
 (defn parse-msg [line]
   (let [parts (str/split line #" ")]
+    ;; FIXME: use map constructor for better readabillity
     (->Message
      (get parts 1)
      (subs (get parts 2) 1)
      (subs (get (str/split (get parts 0) #"!") 0) 1)
-     (subs (str/join " " (drop 3 parts)) 1))))
+     (if (> (count parts) 3)
+       (subs (str/join " " (drop 3 parts)) 1) ""))))
 
-(defn parse [line]
+(defn parse [line, username]
   (cond (str/starts-with? line ":tmi.twitch.tv") nil
-        (str/starts-with? line ":justinfan6969") nil
+        (str/starts-with? line (str ":" username ".tmi.twitch.tv")) nil
         (str/starts-with? line "PING") (->Message "PING" "" "" "")
         :else (parse-msg line)))
 
-;; sending
 (defn send-raw [writer, msg]
   (do
     (.write writer (str msg "\r\n"))
     (.flush writer)))
 
-;; reading
 (defn handle-msg [msg, writer]
   (case (:msg-type msg)
     "PING" (do
-             (println "Answering PING with PONG")
+             (println "[reader] Answering PING with PONG")
              (send-raw writer "PONG :tmi.twitch.tv"))
-    (println msg)))
+    (println (str "[reader] " msg))))
 
-(defn read-loop [reader, writer]
+(defn read-loop [reader, writer, username]
   (loop []
     (let [line (.readLine reader)]
       (if line
         (do
-          (if-let [msg (parse line)]
+          (if-let [msg (parse line username)]
             (handle-msg msg writer))
           (recur))
         (println "No more lines in reader.")))))
 
-;; main
-(defn join [writer]
-  (send-raw writer "PASS oauth: ")
-  (send-raw writer "NICK justinfan6969")
+(defn join [writer, token, username]
+  (send-raw writer (str "PASS oauth:" token))
+  (send-raw writer (str "NICK " username))
   (send-raw writer "JOIN #matthewde"))
 
-(defn run
-  []
+(defn run [config]
   (with-open [socket (Socket. "irc.chat.twitch.tv" 6667)]
     (with-open [writer (jio/writer socket)]
-      (join writer)
+      (join writer (get config "token") (get config "username"))
       (with-open [reader (jio/reader socket)]
-        (read-loop reader writer)))))
+        (read-loop reader writer (get config "username"))))))
+
+(defn read-config []
+  (json/parse-string (slurp "config.json")))
 
 (defn -main
   [& args]
-  (run))
+  (run (read-config)))
 
